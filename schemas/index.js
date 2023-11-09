ca3a96627234497a01029ad49d7d822d2133c95a
Pratik Nath Tiwari <pratik@techalchemy.com>
2023-11-10 00:06:13 +0530

    initial commit

---
+++ schemas/index.js
@@ -0,0 +1,19 @@
+import testimonials from './testimonials.js'
+import abouts from './abouts.js'
+import brands from './brands.js'
+import contact from './contact.js'
+import experiences from './experiences.js'
+import skills from './skills.js'
+import workExperience from './workExperience.js'
+import works from './works.js'
+
+export const schemaTypes = [
+  abouts,
+  brands,
+  contact,
+  experiences,
+  skills,
+  workExperience,
+  works,
+  testimonials,
+]
